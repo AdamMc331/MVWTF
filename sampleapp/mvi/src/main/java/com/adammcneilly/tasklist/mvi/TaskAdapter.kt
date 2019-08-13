a69979c65252ebdecf34465574252c4b84665d14
adammc331 <amcneilly331@gmail.com>
2019-08-13 19:27:45 -0400

    Adding code styles, added KtLint.

--- sampleapp/mvi/src/main/java/com/adammcneilly/tasklist/mvi/TaskAdapter.kt
+++ sampleapp/mvi/src/main/java/com/adammcneilly/tasklist/mvi/TaskAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.adammcneilly.tasklist.mvi.R
 
 class TaskAdapter : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
     var tasks: List<Task> = emptyList()
--- sampleapp/mvi/src/main/java/com/adammcneilly/tasklist/mvi/TaskRepository.kt
+++ sampleapp/mvi/src/main/java/com/adammcneilly/tasklist/mvi/TaskRepository.kt
@@ -1,7 +1,5 @@
 package com.adammcneilly.tasklist.mvi
 
-import com.adammcneilly.tasklist.mvi.Task
-
 interface TaskRepository {
     fun getTasks(): List<Task>
 }
